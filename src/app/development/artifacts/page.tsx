906a169a87ce5cb62a67ca50fbe1f7af58702bac
dai-motoki <dai.motoki1123@gmail.com>
2024-07-13 13:57:21 +0900

    commit

--- src/app/development/artifacts/page.tsx
+++ src/app/development/artifacts/page.tsx
@@ -1,7 +1,17 @@
 'use client'
 
+import Sidebar from '@/components/Sidebar'
 import DynamicComponent from '@/components/DynamicComponent'
 
 export default function ArtifactsPage() {
-  return <DynamicComponent componentName="PackageList" />
+  return (
+    <div className="flex h-screen">
+      {/* サイドバーコンポーネント */}
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto relative bg-gradient-to-b from-gray-100 to-gray-200">
+        {/* パッケージリストを表示するダイナミックコンポーネント */}
+        <DynamicComponent componentName="PackageList" />
+      </main>
+    </div>
+  );
 }
