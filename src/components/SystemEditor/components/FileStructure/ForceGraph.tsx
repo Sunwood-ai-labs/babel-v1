b1d61ebf1d3757e7d585126cc5a2ee45282c4877
dai-motoki <dai.motoki1123@gmail.com>
2024-07-20 01:06:38 +0900

    Update ForceGraph component with 2D/3D toggle

--- src/components/SystemEditor/components/FileStructure/ForceGraph.tsx
+++ src/components/SystemEditor/components/FileStructure/ForceGraph.tsx
@@ -1,9 +1,24 @@
 
-import React, { forwardRef } from 'react';
-import { ForceGraph2D } from 'react-force-graph';
+import React, { forwardRef, useState } from 'react';
+import { ForceGraph2D, ForceGraph3D } from 'react-force-graph';
 
 const ForceGraph = forwardRef((props, ref) => {
-  return <ForceGraph2D ref={ref} {...props} />;
+  const [is3D, setIs3D] = useState(false);
+
+  const toggleDimension = () => {
+    setIs3D(!is3D);
+  };
+
+  const Graph = is3D ? ForceGraph3D : ForceGraph2D;
+
+  return (
+    <div>
+      <button onClick={toggleDimension}>
+        Switch to {is3D ? '2D' : '3D'}
+      </button>
+      <Graph ref={ref} {...props} />
+    </div>
+  );
 });
 
 export default ForceGraph;
